82d7b151077249686b68f9594c7fb8d373ac7796
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-07-11 16:43:29 +0600

    Fetch js

--- Milestone_6/API-/fetch.js
+++ Milestone_6/API-/fetch.js
@@ -1,6 +1,28 @@
 const api = "https://jsonplaceholder.typicode.com/posts";
-console.log(
+/* console.log(
   fetch(api)
     .then((response) => response.json())
     .then((data) => console.log(data)),
-);
+); */
+
+//Button a click korar por load hobe data
+const fetchApi = () => {
+  fetch(api)
+    .then((result) => result.json())
+    .then((output) => console.log(output));
+};
+
+//Button a click korar por akta akta kore load hobe
+
+const arrayOfObj =()=>{
+    fetch(api)
+    .then(result => result.json())
+    .then(output => secondFun(output))
+}
+
+const secondFun =(output)=>{
+    output.forEach(post => {
+        console.log(post);
+        
+    });
+}
